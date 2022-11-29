a1c6d0d981f647f0d706500cf2c8cfbe622d98ee
leonam <leonam447@gmail.com>
2022-11-28 23:46:05 -0300

    att view

--- src/main/java/br/com/leonamCruz/view/MenuPrincipal.java
+++ src/main/java/br/com/leonamCruz/view/MenuPrincipal.java
@@ -1,6 +1,11 @@
 package br.com.leonamCruz.view;
 
 import javax.swing.*;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.util.Date;
 
 public class MenuPrincipal {
     private JTabbedPane alunos;
@@ -9,4 +14,32 @@ public class MenuPrincipal {
     private JButton botaoVamosLa;
     private JComboBox opcNotas;
     private JButton botaoVamosLaNotas;
+    private JPanel root;
+
+    public MenuPrincipal() {
+
+        root.addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                super.componentResized(e);
+                saudacoes.setText(new UtilMenu().saudacoes(new Date()));
+            }
+        });
+    }
+
+    public JPanel getRoot() {
+        return root;
+    }
+
+    public static void main(String[] args) {
+        var menuPrincipal = new MenuPrincipal();
+        var frame = new JFrame("Menu Principal");
+        frame.setContentPane(menuPrincipal.getRoot());
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setLocationRelativeTo(null);
+        frame.setSize(500,175);
+        frame.setVisible(true);
+        frame.requestFocus();
+    }
 }
--- src/main/java/br/com/leonamCruz/view/UtilMenu.java
+++ src/main/java/br/com/leonamCruz/view/UtilMenu.java
@@ -1,9 +1,23 @@
 package br.com.leonamCruz.view;
 
+import java.util.Date;
+
 public class UtilMenu {
 
     public boolean verificaOpcao(short opcaoMaior, short opcaoMenor, short entradaUser){
         return entradaUser <= opcaoMaior && entradaUser >= opcaoMenor;
     }
 
+    public String saudacoes(Date date){
+        int horas = date.getHours();
+        if(horas >= 0 && horas < 6){
+            return "Boa Madrugada... Mas, você deveria está dormindo.";
+        } else if (horas >= 6 && horas < 12) {
+            return "Bom Dia";
+        } else if (horas >=12 && horas < 18) {
+            return "Boa Tarde";
+        } else if (horas >=18 && horas <=23 ) {
+            return "Boa Noite, bora dormir?";
+        } else return "Não consegui acessar seu relógio...";
+    }
 }
